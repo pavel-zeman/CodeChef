92d7355f590f9295ad759876f1f794b0cec6d372
Pavel Zeman <pavel@pavel>
2014-11-17 22:18:41 +0100

    Removed the obsolete November 2014 challenge solutions

--- CodeChef/src/y2014/m11/challenge/ChefAndChuru.cpp
+++ CodeChef/src/y2014/m11/challenge/ChefAndChuru.cpp
@@ -1,4 +1,4 @@
-//
+// Divide the functions into segments of size sqrt(N) and maintain sum for each of these segments. Due to constraints, we have to use unsigned long long (the maximum sum is 10^19).
 // http://www.codechef.com/NOV14/problems/FNCS
 #include <stdio.h>
 #include <string.h>
@@ -10,18 +10,26 @@
 #include <vector>
 #include <map>
 
-#define MAX_N 100010
+#define MAX_N 102000
+#define MAX_SEGMENTS 320
 
 using namespace std;
 
 #define FOR(c, m) for(int c=0;c<(m);c++)
 #define FORE(c, f, t) for(int c=(f);c<(t);c++)
 
-long long int tree[MAX_N];
+unsigned long long int tree[MAX_N];
 int l[MAX_N], r[MAX_N];
 int a[MAX_N];
 int n;
 char c = 0;
+int segmentSize;
+
+vector<pair<int, int> > influencedSegments[MAX_N];
+
+
+unsigned long long segments[MAX_SEGMENTS];
+
 int getInt() {
     int r = 0;
     while (!(c >= '0' && c <= '9')) c = getc_unlocked(stdin);
@@ -32,8 +40,8 @@ int getInt() {
     return r;
 }
 
-long long read(int idx) {
-	long long int sum = 0;
+unsigned long long read(int idx) {
+	unsigned long long int sum = 0;
 	while (idx > 0){
 		sum += tree[idx];
 		idx -= (idx & -idx);
@@ -48,12 +56,51 @@ void update(int idx, long long int val) {
 	}
 }
 
+unsigned long long int calcInterval(int a, int b) {
+    return read(b) - read(a - 1);
+}
+
+unsigned long long int calcSegment(int segment) {
+    unsigned long long int result = 0;
+    int start = segment * segmentSize;
+    int end = start + segmentSize;
+    FORE(i, start, end) result += calcInterval(l[i], r[i]);
+    return result;
+}
+
 int main(void) {
     n = getInt();
-    FOR(i, n) tree[i] = 0;
-    tree[n + 1] = 0;
+    segmentSize = sqrt(n + 1) + 1;
+    FOR(i, n + 3) tree[i] = 0;
     FOR(i, n) update(i + 1, a[i + 1] = getInt());
-    FOR(i, n) l[i] = getInt(), r[i] = getInt();
+    FOR(i, n) {
+        l[i] = getInt();
+        r[i] = getInt();
+    }
+    FORE(i, n, segmentSize * segmentSize) l[i] = r[i] = 0;
+
+    FOR(i, segmentSize) segments[i] = calcSegment(i);
+
+    // pre-alocate space
+    FOR(j, n + 1) influencedSegments[j].reserve(segmentSize);
+
+    FOR(i, segmentSize) {
+        // add segment to the list of influenced segments
+        int used[MAX_N];
+        FOR(j, n + 3) used[j] = 0;
+        int start = segmentSize * i;
+        int end = start + segmentSize;
+        FORE(j, start, end) {
+            used[l[j]]++;
+            used[r[j] + 1]--;
+        }
+        int counter = 0;
+        FOR(j, n + 1) {
+            counter += used[j];
+            if (counter > 0) influencedSegments[j].push_back(make_pair(i, counter));
+        }
+    }
+
     int q = getInt();
     FOR(i, q) {
         int operation = getInt();
@@ -62,16 +109,39 @@ int main(void) {
                 int x = getInt();
                 int y = getInt();
                 update(x, y - a[x]);
+
+                if (y > a[x]) {
+                    unsigned long long int diff = y - a[x];
+                    for (vector<pair<int, int> >::iterator it = influencedSegments[x].begin(); it != influencedSegments[x].end(); it++) segments[it->first] += diff * it->second;
+                } else {
+                    unsigned long long int diff = a[x] - y;
+                    for (vector<pair<int, int> >::iterator it = influencedSegments[x].begin(); it != influencedSegments[x].end(); it++) segments[it->first] -= diff * it->second;
+                }
                 a[x] = y;
                 }
                 break;
             case 2:
                 int m = getInt() - 1;
-                int n = getInt();
-                long long int result = 0;
-                FORE(i, m, n) result += read(r[i]) - read(l[i] - 1);
-                //FORE(i, m, n) printf("%lld ", read(r[i]) - read(l[i] - 1));
-                printf("%lld\n", result);
+                int n = getInt() - 1;
+                unsigned long long int result = 0;
+                if (m % segmentSize != 0) {
+                    int end = (m / segmentSize) * segmentSize + segmentSize - 1;
+                    if (end > n) end = n;
+                    while (m <= end) {
+                        result += calcInterval(l[m], r[m]);
+                        m++;
+                    }
+                }
+                int currentSegment = m / segmentSize;
+                while (m + segmentSize - 1 <= n) {
+                    result += segments[currentSegment++];
+                    m += segmentSize;
+                }
+                while (m <= n) {
+                    result += calcInterval(l[m], r[m]);
+                    m++;
+                }
+                printf("%llu\n", result);
                 break;
         }
     }
--- CodeChef/src/y2014/m11/challenge/Fombinatorial.cpp
+++ CodeChef/src/y2014/m11/challenge/Fombinatorial.cpp
@@ -1,4 +1,4 @@
-// This version works only for N <= 5 and for prime modulos. It is based on the fact, that V(N, R + 1) = V(N, R) * (N - R) ^ (N - R) / (R + 1) ^ (R + 1).
+// Works for all inputs. It is based on the fact, that V(N, R + 1) = V(N, R) * (N - R) ^ (N - R) / (R + 1) ^ (R + 1). Stores current powers of all prime factors in an array and lazily calculates required result.
 // http://www.codechef.com/NOV14/problems/POWERMUL
 #include <stdio.h>
 #include <string.h>
@@ -11,6 +11,9 @@
 #include <map>
 
 #define MAX_N 100010
+#define MAX_PRIMES 10000
+#define GROUP_SIZE 64
+#define GROUPS ((MAX_PRIMES / GROUP_SIZE) + 1)
 
 using namespace std;
 
@@ -29,18 +32,7 @@ int getInt() {
     return r;
 }
 
-long long int power(int a, int b) {
-    long long int result = 1;
-    long long int temp = a;
-    while (b > 0) {
-        if ((b & 1) == 1) result *= temp;
-        temp *= temp;
-        b >>= 1;
-    }
-    return result;
-}
-
-long long int power(int a, int b, long long int mod) {
+long long int power(int a, long long int b, long long int mod) {
     long long int result = 1;
     long long int temp = a;
     while (b > 0) {
@@ -51,41 +43,107 @@ long long int power(int a, int b, long long int mod) {
     return result;
 }
 
-
-long long int f(int n) {
-    long long int result = 1;
-    FOR(i, n) result *= power(i + 1, i + 1);
-    return result;
-}
-
-long long int inv(long long int value, long long int mod) {
-    return power(value, mod - 2, mod);
-}
-
-long long int fData[MAX_N];
-
+bool prime[MAX_N];
+int primeCount = 0;
+int primes[MAX_PRIMES];
+int primePosition[MAX_N];
+vector<pair<int, int> > primeList[MAX_N];
 
 int main(void) {
+    // generate all primes lower than 10^5
+    FOR(i, MAX_N) prime[i] = true;
+    FORE(i, 2, MAX_N) {
+        if (prime[i]) for(int next = i + i; next < MAX_N; next += i) prime[next] = false;
+    }
+    int primeCount = 0;
+    FORE(i, 2, MAX_N) if (prime[i]) {
+        primes[primeCount] = i;
+        primePosition[i] = primeCount++;
+    } else primePosition[i] = primeCount;
+
+    // generate list of primes for each number
+    FORE(i, 2, MAX_N) {
+        int j = 0;
+        int temp = i;
+        while (primes[j] * primes[j] <= temp) {
+            int count = 0;
+            while (temp % primes[j] == 0) {
+                count++;
+                temp /= primes[j];
+            }
+            if (count > 0) primeList[i].push_back(make_pair(j, count * i));
+            j++;
+        }
+        if (temp > 1) primeList[i].push_back(make_pair(primePosition[temp], 1 * i));
+    }
+
+
+
     int t = getInt();
     FOR(tt, t) {
         int n = getInt();
         int m = getInt();
         int q = getInt();
-
-        if (n > 5) {
-            // assume prime
-            fData[0] = 1;
-            fData[1] = power(n, n, m);
-            FORE(i, 2, n) {
-                long long int temp = power(i, i, m);
-                fData[i] = fData[i - 1] * power(n - i + 1, n - i + 1, m) % m * inv(temp, m) % m;
-            }
+        bool used[MAX_N];
+        bool dirty[MAX_PRIMES];
+        bool dirtyGroup[GROUPS];
+        int result[MAX_N];
+        int primeResult[MAX_PRIMES];
+        int primeGroupResult[GROUPS];
+        long long int currentPrimeCount[MAX_PRIMES];
+        vector<int> input;
+        int usedSize = (n >> 1) + 2;
+        FOR(i, usedSize) used[i] = false;
+        int maxPrimes = primePosition[n] + 1;
+        FOR(i, maxPrimes) {
+            currentPrimeCount[i] = 0;
+            dirty[i] = false;
+        }
+        int maxGroups = maxPrimes / GROUP_SIZE + 1;
+        FOR(i, maxGroups) {
+            dirtyGroup[i] = false;
+            primeGroupResult[i] = 1;
         }
 
         FOR(qq, q) {
             int r = getInt();
-            if (n <= 5) printf("%lld\n", f(n) / f(r) / f(n - r) % m);
-            else printf("%lld\n", fData[r]); // assume prime
+            input.push_back(r);
+            if (r > n - r) r = n - r;
+            used[r] = true;
+        }
+
+        for(int i=1;i <= n - i;i++) {
+            int p = n - i + 1;
+            // update current prime powers
+            if (p > 1) for (vector<pair<int, int> >::iterator it = primeList[p].begin(); it != primeList[p].end(); it++) currentPrimeCount[it->first] += it->second, dirty[it->first] = true, dirtyGroup[it->first / GROUP_SIZE] = true;
+            if (i > 1) for (vector<pair<int, int> >::iterator it = primeList[i].begin(); it != primeList[i].end(); it++) currentPrimeCount[it->first] -= it->second, dirty[it->first] = true, dirtyGroup[it->first / GROUP_SIZE] = true;
+            if (used[i]) {
+                // we need to calculate this value
+                long long int local = 1;
+                FOR(j, maxGroups) {
+                    // try a group first - if it is dirty, calculate its value
+                    if (dirtyGroup[j]) {
+                        long long int local2 = 1;
+                        int end = (j + 1) * GROUP_SIZE;
+                        FORE(k, j * GROUP_SIZE, end) if (currentPrimeCount[k] > 0) {
+                            if (dirty[k]) {
+                                primeResult[k] = power(primes[k], currentPrimeCount[k], m);
+                                dirty[k] = false;
+                            }
+                            local2 = local2 * primeResult[k] % m;
+                        }
+                        primeGroupResult[j] = local2;
+                        dirtyGroup[j] = false;
+                    }
+                    local = local * primeGroupResult[j] % m;
+                }
+                result[i] = local;
+            }
+        }
+
+        for(vector<int>::iterator it = input.begin(); it != input.end(); it++) {
+            int r = *it;
+            printf("%d\n", result[r <= n - r ? r : n - r]);
         }
     }
 }
