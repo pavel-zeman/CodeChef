8601e8f7a07c7f59615c499080e23f8f719f5303
Pavel Zeman <pavel@pavel.cz>
2018-11-03 20:02:29 +0100

    Added solutions of 2018 Snackdown qualification

---
+++ SnackDown/y2018/Qualifier/ChefAndSemiPrimes.cpp
@@ -0,0 +1,133 @@
+// Use brute force
+// https://www.codechef.com/SNCKQL19/problems/CHEFPRMS
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FOR(c, m) for(int c=0;c<(int)(m);c++)
+#define FORE(c, f, t) for(int c=(f);c<(int)(t);c++)
+#define LLI long long int
+#define MOD 1000000007
+
+
+char ccc = 0;
+int getInt() {
+    int r = 0;
+    while (!(ccc >= '0' && ccc <= '9')) ccc = getc_unlocked(stdin);
+    while (ccc >= '0' && ccc <= '9') {
+        r = r * 10 + (ccc - '0');
+        ccc = getc_unlocked(stdin);
+    }
+    return r;
+}
+
+int getString(char *start) {
+    char *data = start;
+    while (!(ccc > ' ' && ccc <= '~')) ccc = getc_unlocked(stdin);
+    while (ccc > ' ' && ccc <= '~') {
+        *data++ = ccc;
+        ccc = getc_unlocked(stdin);
+    }
+    *data = 0;
+    return data - start;
+}
+
+
+int getSignedInt() {
+    int r = 0;
+    while (!(ccc == '-' || (ccc >= '0' && ccc <= '9'))) ccc = getc_unlocked(stdin);
+    bool minus = ccc == '-';
+    if (minus) ccc = getc_unlocked(stdin);
+    while (ccc >= '0' && ccc <= '9') {
+        r = r * 10 + (ccc - '0');
+        ccc = getc_unlocked(stdin);
+    }
+    return minus ? -r : r;
+}
+
+long long int getLongLongInt() {
+    long long int r = 0;
+    while (!(ccc >= '0' && ccc <= '9')) ccc = getc_unlocked(stdin);
+    while (ccc >= '0' && ccc <= '9') {
+        r = r * 10 + (ccc - '0');
+        ccc = getc_unlocked(stdin);
+    }
+    return r;
+}
+
+
+template <class type> void print(type a) {
+    if (a < 0) {
+        putc_unlocked('-', stdout);
+        a = -a;
+    }
+    if (a == 0) {
+        putc_unlocked('0', stdout);
+    } else {
+        char result[20];
+        int resSize = 0;
+        while (a > 0) {
+            result[resSize++] = '0' + a % 10;
+            a /= 10;
+        }
+        while (--resSize >= 0) putc_unlocked(result[resSize], stdout);
+    }
+    putc_unlocked('\n', stdout);
+}
+
+void printString(const char *str) {
+    while (*str) putc_unlocked(*str++, stdout);
+    putc_unlocked('\n', stdout);
+}
+
+int power(int a, int b) {
+    int sq = a;
+    int result = 1;
+    while (b > 0) {
+        if ((b & 1) == 1) result = result * (LLI)sq % MOD;
+        sq = sq * (LLI)sq % MOD;
+        b >>= 1;
+    }
+    return result;
+}
+
+int inv(int a) {
+    return power(a, MOD - 2);
+}
+
+template <class type> type gcd(type a, type b) {
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+void fail() {
+    *((char *)0) = 0;
+}
+
+double cpu() {
+    return clock() / (double)CLOCKS_PER_SEC;
+}
+
+#define MAX_N 210
+int primes[] = {2, 	3 ,	5 ,	7 ,	11 ,	13 ,	17 ,	19 ,	23,
+29, 	31 ,	37 ,	41 ,	43 ,	47 ,	53 ,	59 ,	61 ,	67,
+71, 	73 ,	79 ,	83 ,	89 ,	97 ,	101 ,	103 };
+bool semiOk[MAX_N];
+bool ok[MAX_N];
+
+int main(void) {
+    FOR(i, sizeof(primes) / sizeof(primes[0]))
+        FORE(j, i + 1, sizeof(primes) / sizeof(primes[0])) {
+            if (primes[i] * primes[j] >= MAX_N) break;
+            semiOk[primes[i] * primes[j]] = true;
+        }
+    FOR(i, MAX_N)
+        if (semiOk[i])
+            FOR(j, MAX_N)
+                if (semiOk[j] && i + j < MAX_N) ok[i + j] = true;
+
+    int t = getInt();
+    FOR(tt, t) {
+        int n = getInt();
+        printString(ok[n] ? "YES" : "NO");
+    }
+}
+
